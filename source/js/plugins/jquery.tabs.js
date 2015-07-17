35fb0883b0fb3cfa66ea2441196ab11d4a5bad86
Zoxon <zoxon.box@gmail.com>
2015-07-17 13:26:24 +0600

    Пока для табов будет этот говнокод, дальше посмотрим

---
+++ source/js/plugins/jquery.tabs.js
@@ -0,0 +1,55 @@
+(function($) {
+
+	/*!
+	 * jQuery spinner plugin
+	 *
+	 * Copyright (c) 2014 Weltkind Web Works
+	 * www.weltkind.com
+	 *
+	 * @version 1.0.0
+	 *
+	 */
+
+
+	$('.b-tabs .b-tabs__body-content').css({
+		"opacity": "0",
+		"display": "none"
+	});
+
+	$('.b-tabs').each(function() {
+
+		$(this).find('.b-tabs__body-content').first().css({
+			"opacity": "1",
+			"padding-left": "0",
+			"display": "block"
+		});
+
+		$(this).find('.b-tabs__head-item').first().addClass("b-tabs__head-item_active");
+
+	});
+
+	$(".b-tabs__head a").click(function(event) {
+		event.preventDefault();
+		$parent = $(this).parents('.b-tabs');
+
+		$(this).parent().addClass("b-tabs__head-item_active");
+		$(this).parent().siblings().removeClass("b-tabs__head-item_active");
+
+
+		var tab = $(this).attr("href");
+		$parent.find(".b-tabs__body-content").not(tab).css({
+			// "padding-left": "20px",
+			"opacity": 0,
+			"display": "none"
+		});
+
+
+		$(tab).css("display", "block").animate({
+			paddingLeft: 0,
+			opacity: 1
+		}, 400);
+	});
+
+
+
+})(jQuery);
