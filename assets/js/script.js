c4d45e220282f795c19326e13e659336b9da750e
Zoxon <zoxon.box@gmail.com>
2015-08-07 09:59:59 +0600

    Загрузка через модернизр

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,45 @@
+Modernizr.load([
+	{
+		test: Modernizr.placeholder,
+		nope: 'js/placeholders.min.js'
+	},
+	{
+		test: Modernizr.touch,
+		yep: ['js/fastclick.min.js'],
+		nope: [
+			'js/jquery.formstyler.min.js',
+			'css/formstyler.css'
+		],
+		complete: function(){
+			if (Modernizr.touch) {
+				FastClick.attach(document.body);
+			}
+			else {
+				// Form styler
+				$('select, input').styler();
+
+				// Colorbox
+				// $('a.js-cbox-modal').colorbox({
+				// 	title: " ",
+				// 	previous: false,
+				// 	next: false,
+				// 	arrowKey: false,
+				// 	rel: false,
+				// 	overlayClose: true,
+				// 	opacity: 0.8,
+				// 	maxWidth: '100%',
+				// 	maxHeight: '100%',
+				// 	onComplete: function() {
+				// 		$('input, select').styler();
+				// 		$.colorbox.resize();
+				// 	}
+				// });
+			}
+		}
+	}
+]);
+
+
 $(document).ready(function() {
-	// Form Styler
-	// $('select, input').styler();
-
-	//Placeholder
-	// $('.js-input-placeholder').placeholder();
-
-	//Colorbox
-	// $('a.js-cbox-modal').colorbox({
-	// 	title: " ",
-	// 	previous: false,
-	// 	next: false,
-	// 	arrowKey: false,
-	// 	rel: false,
-	// 	overlayClose: true,
-	// 	opacity: 0.8,
-	// 	onComplete: function() {
-	// 		$('input, select').styler();
-	// 		$.colorbox.resize();
-	// 	}
-	// });
 
 });
