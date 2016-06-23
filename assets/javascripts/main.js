25b66f160cef6c3656cf9af7c1a64af9a76fc0fb
Velichko Konstantin <zoxon.box@gmail.com>
2016-06-23 16:27:02 +0600

    Update 2016-06-23T10:26:00.566Z

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(function() {
 	svg4everybody();
 
@@ -7,303 +9,330 @@ $(document).ready(function() {
 
 	// Here insert modules scripts
 	// Accordion
-	$('.accordion').each(function(index, el) {
-		var $that = $(this);
-		var $items = $(this).find('.accordion__item');
-		var $headers = $(this).find('.accordion__header');
-		var $contents = $(this).find('.accordion__content');
-		var speed = 300;
-	
-		$items.each(function(index, el) {
-			var findActive = false;
-	
-			if (!findActive) {
-				if ($(this).hasClass('accordion__item_active')) {
-					$(this).children('.accordion__content').show();
-					return false;
-				}
-			}
-			else {
-				$(this).removeClass('accordion__item_active');
-			}
-		});
-	
-		$headers.click(function(event) {
-			event.preventDefault();
-	
-			var $item = $(this).parent();
-	
-			if (!$item.hasClass('accordion__item_active')) {
-				$items.removeClass('accordion__item_active');
-				$item.addClass('accordion__item_active');
-				$contents.slideUp(speed);
-				$item.children('.accordion__content').slideDown(speed);
-			}
-			else {
-				$item.children('.accordion__content').slideUp(speed);
-				$item.removeClass('accordion__item_active');
-			}
-		});
-	});
-	
-	
-	$('.alert__close').on('click', function() {
-		$(this).closest('.alert').fadeOut();
-	});
-	
-	
-	$('.browsehappy').click(function() {
-		$(this).slideUp();
-	});
-	
-	
-	
-	var setFormFieldState = function(block, control) {
-		var $block = $('.' + block);
-	
-		$block.each(function(index, el) {
+	(function() {
+		$('.accordion').each(function(index, el) {
 			var $that = $(this);
-			var $control = $that.find('.' + block + '__' + control);
+			var $items = $(this).find('.accordion__item');
+			var $headers = $(this).find('.accordion__header');
+			var $contents = $(this).find('.accordion__content');
+			var speed = 300;
 	
-			// Focus
-			$control.focusin(function(event) {
-				$(this).parents('.' + block).addClass(block + '_focused');
+			$items.each(function(index, el) {
+				var findActive = false;
+	
+				if (!findActive) {
+					if ($(this).hasClass('accordion__item_active')) {
+						$(this).children('.accordion__content').show();
+						return false;
+					}
+				}
+				else {
+					$(this).removeClass('accordion__item_active');
+				}
 			});
 	
-			$control.focusout(function(event) {
-				$(this).parents('.' + block).removeClass(block + '_focused');
-			});
+			$headers.click(function(event) {
+				event.preventDefault();
 	
-			// Checked
-			if ($control.is(':checked')) {
-				$that.addClass(block + '_checked');
-			}
+				var $item = $(this).parent();
 	
-			$control.click(function(event) {
-				$('input:not(:checked)').parents('.' + block).removeClass(block + '_checked');
-				$('input:checked').parents('.' + block).addClass(block + '_checked');
-			});
-	
-			// Disabled
-			if ($control.is(':disabled')) {
-				$that.addClass(block + '_disabled');
-			}
-	
-			// Multiple
-			if ($control.is('[multiple]')) {
-				$that.addClass(block + '_multiple');
-			}
-	
-			$that.addClass(block + '_inited');
-		});
-	};
-	
-	
-	setFormFieldState('select', 'control');
-	setFormFieldState('input', 'control');
-	setFormFieldState('textarea', 'control');
-	setFormFieldState('checkbox', 'control');
-	setFormFieldState('radio', 'control');
-	
-	
-	$('input[type="file"]').each(function(index, el) {
-		var $that = $(this);
-		$that.change(function(event) {
-			$(this).each(function(index, el) {
-				console.log(el.files[index].name);
+				if (!$item.hasClass('accordion__item_active')) {
+					$items.removeClass('accordion__item_active');
+					$item.addClass('accordion__item_active');
+					$contents.slideUp(speed);
+					$item.children('.accordion__content').slideDown(speed);
+				}
+				else {
+					$item.children('.accordion__content').slideUp(speed);
+					$item.removeClass('accordion__item_active');
+				}
 			});
 		});
-	});
+	})();
 	
 	
-	var $header    = $('.header')
-		$headerTop = $header.find('.header__top'),
-		$window    = $(window),
-		marginTop  = 0,
-		offset     = $headerTop.offset().top,
-		classAdded = false;
-	
-	$window.scroll(function() {
-		if ($window.scrollTop() > offset) {
-			if (classAdded === false) {
-				$header.addClass('header_fixed');
-			}
-			classAdded = true;
-		}
-		else {
-			$header.removeClass('header_fixed');
-			classAdded = false;
-		}
-	});
-	
-	
-	$('.js-link-sroll a[href^="#"]').on('click',function (event) {
-		event.preventDefault();
-		var $heder = $('.header__top');
-		var target = this.hash;
-		var $target = $(target);
-	
-		if ($(this).parent().hasClass('main-menu-submenu__item')) {
-			$('.main-menu-submenu__item').removeClass('main-menu-submenu__item_active');
-			$(this).parent().addClass('main-menu-submenu__item_active');
-		}
-	
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top - $heder.outerHeight()
-		}, 900, 'swing', function () {
-			window.location.hash = target;
+	// Browsehappy
+	(function() {
+		$('.browsehappy').click(function() {
+			$(this).slideUp();
 		});
-	});
-	
-	$('.logo').click(function(event) {
-		event.preventDefault();
-	
-		$('html, body').stop().animate({
-			'scrollTop': 0
-		}, 900, 'swing');
-	});
+	})();
 	
 	
-	var mmenu = {
-		trigger: $('.mobile-menu-trigger'),
-		panel: $('.page__mobile-menu'),
-		pageContent: $('.page__content'),
-		cover: true,
-		menuPosition: 'right',
+	// Alert
+	(function() {
+		$('.alert__close').on('click', function() {
+			$(this).closest('.alert').fadeOut();
+		});
+	})();
 	
-		show: function(){
-			mmenu.panel.show();
-			var menuWidth = mmenu.panel.width();
 	
-			if (mmenu.menuPosition === 'right') {
-				var menuWidth = '-' + menuWidth;
-			}
+	// Forms
+	(function() {
+		var setFormFieldState = function(block, control) {
+			var $block = $('.' + block);
 	
-			// mmenu.pageContent.transition({
-			// 	x: menuWidth + 'px',
-			// 	complete: function(){
-			// 		$('.page').addClass('page_mmenu-open');
-			// 		$('html, body').css({
-			// 			"overflow": "hidden",
-			// 			"height": "auto"
-			// 		});
-			// 	}
-			// });
-			$('.page').addClass('page_mmenu-open', function(){
-				$('.page').addClass('page_mmenu-open');
-				$('html, body').css({
-					"overflow": "hidden",
-					"height": "auto"
+			$block.each(function(index, el) {
+				var $that = $(this);
+				var $control = $that.find('.' + block + '__' + control);
+	
+				// Focus
+				$control.focusin(function(event) {
+					$(this).parents('.' + block).addClass(block + '_focused');
 				});
-			});
-		},
 	
-		hide: function(){
-			$('.page').removeClass('page_mmenu-open', function(){
-					mmenu.panel.hide();
-					$('html, body').removeAttr('style');
-					$(this).removeAttr('style');
+				$control.focusout(function(event) {
+					$(this).parents('.' + block).removeClass(block + '_focused');
 				});
-			// mmenu.pageContent.transition({
-			// 	x: 0,
-			// 	complete: function(){
-			// 		mmenu.panel.hide();
-			// 		$('html, body').removeAttr('style');
-			// 		$(this).removeAttr('style');
-			// 	}
-			// });
-		}
-	};
 	
-	if (mmenu.cover) {
-		mmenu.pageContent.append('<div class="page__cover">');
-	}
+				// Checked
+				if ($control.is(':checked')) {
+					$that.addClass(block + '_checked');
+				}
 	
-	mmenu.trigger.click(function(event) {
-		event.preventDefault();
-		mmenu.show();
-	});
+				$control.click(function(event) {
+					$('input:not(:checked)').parents('.' + block).removeClass(block + '_checked');
+					$('input:checked').parents('.' + block).addClass(block + '_checked');
+				});
 	
-	$('.page__cover').on('click', function(event) {
-		event.preventDefault();
+				// Disabled
+				if ($control.is(':disabled')) {
+					$that.addClass(block + '_disabled');
+				}
 	
-		if ($('.page').hasClass('page_mmenu-open')) {
+				// Multiple
+				if ($control.is('[multiple]')) {
+					$that.addClass(block + '_multiple');
+				}
+	
+				$that.addClass(block + '_inited');
+			});
+		};
+	
+	
+		setFormFieldState('select', 'control');
+		setFormFieldState('input', 'control');
+		setFormFieldState('textarea', 'control');
+		setFormFieldState('checkbox', 'control');
+		setFormFieldState('radio', 'control');
+	
+	
+		$('.input').each(function(index, el) {
+			var $input = $(this);
+			var $box = $input.find('.input__box');
+			var $control = $input.find('input[type="file"]');
+			var placeholder = {
+				file: $control.attr('placeholder') || 'Select file...',
+				btn: 'Browse',
+				multiple: 'Files selected: '
+			}
+	
+			if ($control.length > 0) {
+				$input.addClass('input_inited input_type_file');
+	
+				var $fileBtn = $('<span class="input__file-btn">' + placeholder.btn + '</span>').appendTo($box);
+				var $fileName = $('<span class="input__file-name">' + placeholder.file + '</span>').appendTo($box);
+	
+				$fileBtn.add($fileName).on('click', function() {
+					$(this).parents('.input').find('input[type="file"]').click();
+				});
+			}
+	
+			$control.change(function(event) {
+				// Single file
+				if (this.files.length == 1) {
+					$fileName.text(this.files[0].name);
+				}
+				// Multiple files
+				else if (this.files.length > 1) {
+					$fileName.text(placeholder.multiple + this.files.length);
+				}
+			});
+		});
+	
+	})();
+	
+	
+	// Header
+	(function() {
+		var $header    = $('.header');
+		var $headerTop = $header.find('.header__top');
+		var $window    = $(window);
+		var marginTop  = 0;
+		var offset     = $headerTop.offset().top;
+		var classAdded = false;
+	
+		$window.scroll(function() {
+			if ($window.scrollTop() > offset) {
+				if (classAdded === false) {
+					$header.addClass('header_fixed');
+				}
+				classAdded = true;
+			}
+			else {
+				$header.removeClass('header_fixed');
+				classAdded = false;
+			}
+		});
+	
+	
+		$('.js-link-sroll a[href^="#"]').on('click',function (event) {
 			event.preventDefault();
-			mmenu.hide();
+			var $heder = $('.header__top');
+			var target = this.hash;
+			var $target = $(target);
+	
+			if ($(this).parent().hasClass('main-menu-submenu__item')) {
+				$('.main-menu-submenu__item').removeClass('main-menu-submenu__item_active');
+				$(this).parent().addClass('main-menu-submenu__item_active');
+			}
+	
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top - $heder.outerHeight()
+			}, 900, 'swing', function () {
+				window.location.hash = target;
+			});
+		});
+	
+		$('.logo').click(function(event) {
+			event.preventDefault();
+	
+			$('html, body').stop().animate({
+				'scrollTop': 0
+			}, 900, 'swing');
+		});
+	})();
+	
+	
+	// Forms
+	(function() {
+		var mmenu = {
+			trigger: $('.mobile-menu-trigger'),
+			panel: $('.page__mobile-menu'),
+			pageContent: $('.page__content'),
+			cover: true,
+			menuPosition: 'right',
+	
+			show: function(){
+				mmenu.panel.show();
+				var menuWidth = mmenu.panel.width();
+	
+				if (mmenu.menuPosition === 'right') {
+					var menuWidth = '-' + menuWidth;
+				}
+	
+				$('.page').addClass('page_mmenu-open', function(){
+					$('.page').addClass('page_mmenu-open');
+					$('html, body').css({
+						"overflow": "hidden",
+						"height": "auto"
+					});
+				});
+			},
+	
+			hide: function(){
+				$('.page').removeClass('page_mmenu-open', function(){
+						mmenu.panel.hide();
+						$('html, body').removeAttr('style');
+						$(this).removeAttr('style');
+					});
+			}
+		};
+	
+		if (mmenu.cover) {
+			mmenu.pageContent.append('<div class="page__cover">');
 		}
-	});
+	
+		mmenu.trigger.click(function(event) {
+			event.preventDefault();
+			mmenu.show();
+		});
+	
+		$('.page__cover').on('click', function(event) {
+			event.preventDefault();
+	
+			if ($('.page').hasClass('page_mmenu-open')) {
+				event.preventDefault();
+				mmenu.hide();
+			}
+		});
+	})();
 	
 	
-	// Remodal init
-	$('[data-remodal-id]').remodal();
+	// Remodal
+	(function() {
+		$('[data-remodal-id]').remodal();
+	})();
 	
 	
 	// Dropdown
-	$('.pseudo-dropdown').each(function(index, el) {
-		var $that = $(this);
-		var $trigger = $(this).find('.pseudo-dropdown__trigger');
-		var $list = $(this).find('.pseudo-dropdown-list__item');
-		var $active = $(this).find('.pseudo-dropdown-list__item_active');
-		var $dropdown = $(this).find('.pseudo-dropdown__list');
-	
-		var $triggerText = '';
-	
-		// Set initial state
-		if ($active.length > 0) {
-			$triggerText = $active.text();
-		}
-		else {
-			$triggerText = $list.first().text()
-		}
-	
-		$trigger.text($triggerText);
-	
-		// Show dorpdown
-		if (!$that.hasClass('pseudo-dropdown_dropdown_open')) {
-			$trigger.click(function(event) {
-				event.preventDefault();
-				// Close all open dropdowns
-				$('.pseudo-dropdown').removeClass('pseudo-dropdown_dropdown_open');
-				$that.toggleClass('pseudo-dropdown_dropdown_open');
-			});
-		}
-	
-		// Dropdown item click
-		$list.click(function(event) {
-			event.preventDefault();
+	(function() {
+		$('.pseudo-dropdown').each(function(index, el) {
+			var $that = $(this);
+			var $trigger = $(this).find('.pseudo-dropdown__trigger');
+			var $list = $(this).find('.pseudo-dropdown-list__item');
+			var $active = $(this).find('.pseudo-dropdown-list__item_active');
+			var $dropdown = $(this).find('.pseudo-dropdown__list');
 	
 			var $triggerText = '';
 	
-			if ($(this).is('li') && $(this).text() != '') {
-				$triggerText = $(this).text();
+			// Set initial state
+			if ($active.length > 0) {
+				$triggerText = $active.text();
 			}
 			else {
-				$triggerText = $(this).children('a').text();
+				$triggerText = $list.first().text()
 			}
+	
 			$trigger.text($triggerText);
 	
-			$list.removeClass('pseudo-dropdown-list__item_active');
-			$(this).addClass('pseudo-dropdown-list__item_active');
-	
-			$that.toggleClass('pseudo-dropdown_dropdown_open');
-	
-			if ($(this).attr('data-value').length > 0) {
-				alert('Selected: "' + $(this).attr('data-value') + '" (' + $triggerText + ')');
-			}
-		});
-	
-		// Outer click
-		$(document).on('click', function(event) {
-			var closeClassName = 'pseudo-dropdown';
-			var $hideObject = $('.pseudo-dropdown__list');
-	
-			if (!$(event.target).closest('.'+closeClassName).length) {
-				if ($hideObject.is(":visible")) {
+			// Show dorpdown
+			if (!$that.hasClass('pseudo-dropdown_dropdown_open')) {
+				$trigger.click(function(event) {
+					event.preventDefault();
+					// Close all open dropdowns
 					$('.pseudo-dropdown').removeClass('pseudo-dropdown_dropdown_open');
-				}
+					$that.toggleClass('pseudo-dropdown_dropdown_open');
+				});
 			}
+	
+			// Dropdown item click
+			$list.click(function(event) {
+				event.preventDefault();
+	
+				var $triggerText = '';
+	
+				if ($(this).is('li') && $(this).text() != '') {
+					$triggerText = $(this).text();
+				}
+				else {
+					$triggerText = $(this).children('a').text();
+				}
+				$trigger.text($triggerText);
+	
+				$list.removeClass('pseudo-dropdown-list__item_active');
+				$(this).addClass('pseudo-dropdown-list__item_active');
+	
+				$that.toggleClass('pseudo-dropdown_dropdown_open');
+	
+				if ($(this).attr('data-value').length > 0) {
+					alert('Selected: "' + $(this).attr('data-value') + '" (' + $triggerText + ')');
+				}
+			});
+	
+			// Outer click
+			$(document).on('click', function(event) {
+				var closeClassName = 'pseudo-dropdown';
+				var $hideObject = $('.pseudo-dropdown__list');
+	
+				if (!$(event.target).closest('.'+closeClassName).length) {
+					if ($hideObject.is(":visible")) {
+						$('.pseudo-dropdown').removeClass('pseudo-dropdown_dropdown_open');
+					}
+				}
+			});
 		});
-	});
+	})();
 	
 
 });
--- assets/javascripts/plugins.js
+++ assets/javascripts/plugins.js
@@ -4144,15 +4144,21 @@ This test will also return `true` for Firefox 4 Multitouch support.
 
 (function($) {
 	$.fn.basictable = function(options) {
+
 		var setup = function(table, data) {
+			var headings = [];
+
 			if (data.tableWrap) {
-				table.wrap('<div class="' + settings.baseClass + '__wrapper"></div>');
+				table.wrap('<div class="' + data.baseClass + '__wrapper"></div>');
 			}
 
 			var format = '';
 
 			if (table.find('thead tr th').length) {
-				format = 'thead tr th';
+				format = 'thead th';
+			}
+			else if (table.find('tbody tr th').length) {
+				format = 'tbody tr th';
 			}
 			else if (table.find('th').length) {
 				format = 'tr:first th';
@@ -4160,8 +4166,6 @@ This test will also return `true` for Firefox 4 Multitouch support.
 			else {
 				format = 'tr:first td';
 			}
-
-			var headings = [];
 
 			$.each(table.find(format), function() {
 				var $heading = $(this);
@@ -4177,44 +4181,50 @@ This test will also return `true` for Firefox 4 Multitouch support.
 				}
 			});
 
+			// Table Body
 			$.each(table.find('tbody tr'), function() {
-				var $row = $(this);
-				var cellIndex = 0;
+				setupRow($(this), headings, data);
+			});
 
-				$row.children().each(function() {
-					var $cell = $(this);
+			// Table Footer
+			$.each(table.find('tfoot tr'), function() {
+				setupRow($(this), headings, data);
+			});
+		};
 
-					if ($cell.html().trim() === '' || $cell.html() === '&nbsp;') {
-						$cell.addClass(settings.baseClass + '__hide');
-					}
-					else {
-						for (var i = 0; i < headings.length; i++) {
-							var $heading = $(headings[i][cellIndex]);
+		var setupRow = function($row, headings, data) {
+			$row.children().each(function() {
+				var $cell = $(this);
 
-							if ($heading.html() !== '' && $heading.html() !== '&nbsp;') {
-								if( $cell.attr('data-th') ) {
-									$cell.attr('data-th', $cell.attr('data-th') + ': ' + $heading.text());
-								}
-								else {
-									$cell.attr('data-th', $heading.text());
-								}
+				if (($cell.html() === '' || $cell.html() === '&nbsp;') && (!data.showEmptyCells)) {
+					$cell.addClass(data.baseClass + '__hide');
+				}
+				else {
+					var cellIndex = $cell.index();
+					var headingText = '';
 
-								if (data.contentWrap && !$cell.children().hasClass(settings.baseClass + '__content')) {
-										$cell.wrapInner('<span class="' + settings.baseClass + '__content" />');
-								}
-							}
+					for (var j = 0; j < headings.length; j++) {
+						if (j != 0) {
+							headingText += ': ';
 						}
+
+						var $heading = headings[j][cellIndex];
+						headingText += $heading.text();
 					}
 
-					cellIndex += $cell.attr('colspan') || 1;
-				});
+					$cell.attr('data-th', headingText);
+
+					if (data.contentWrap && !$cell.children().hasClass(data.baseClass + '__content')) {
+						$cell.wrapInner('<span class="' + data.baseClass + '__content" />');
+					}
+				}
 			});
 		};
 
 		var unwrap = function(table) {
 			$.each(table.find('td'), function() {
 				var $cell = $(this);
-				var content = $cell.children('.' + settings.baseClass + '__content').html();
+				var content = $cell.children('.' + data.baseClass + '__content').html();
 				$cell.html(content);
 			});
 		};
@@ -4223,7 +4233,7 @@ This test will also return `true` for Firefox 4 Multitouch support.
 			// Only change when table is larger than parent if force
 			// responsive is turned off.
 			if (!data.forceResponsive) {
-				if (table.removeClass(settings.baseClass + '_responsive').outerWidth() > table.parent().width()) {
+				if (table.removeClass(data.baseClass + '_responsive').outerWidth() > table.parent().width()) {
 					start(table, data);
 				}
 				else {
@@ -4241,18 +4251,18 @@ This test will also return `true` for Firefox 4 Multitouch support.
 		};
 
 		var start = function(table, data) {
-			table.addClass(settings.baseClass + '_responsive');
+			table.addClass(data.baseClass + '_responsive');
 
-			if (data.tableWrapper) {
-				table.parent('.' + settings.baseClass + 'wrapper').addClass(settings.baseClass + '__wrapper_active');
+			if (data.tableWrap) {
+				table.parent('.' + data.baseClass + 'wrapper').addClass(data.baseClass + '__wrapper_active');
 			}
 		};
 
 		var end = function(table, data) {
-			table.removeClass(settings.baseClass + '_responsive');
+			table.removeClass(data.baseClass + '_responsive');
 
-			if (data.tableWrapper) {
-				table.parent('.' + settings.baseClass + '__wrapper').removeClass(settings.baseClass + '__wrapper_active');
+			if (data.tableWrap) {
+				table.parent('.' + data.baseClass + '__wrapper').removeClass(data.baseClass + '__wrapper_active');
 			}
 		};
 
@@ -4277,52 +4287,56 @@ This test will also return `true` for Firefox 4 Multitouch support.
 		};
 
 		// Get table.
-		var table = this;
+		this.each(function() {
+			var table = $(this);
 
-		// If table has already executed.
-		if (table.length === 0 || table.data('basictable')) {
-			if (table.data('basictable')) {
-				// Destroy basic table.
-				if (options == 'destroy') {
-					destroy(table, table.data('basictable'));
+			// If table has already executed.
+			if (table.length === 0 || table.data('basictable')) {
+				if (table.data('basictable')) {
+					// Destroy basic table.
+					if (options == 'destroy') {
+						destroy(table, table.data('basictable'));
+					}
+					// Start responsive mode.
+					else if (options === 'start') {
+						start(table, table.data('basictable'));
+					}
+					else if (options === 'stop') {
+						end(table, table.data('basictable'));
+					}
+					else {
+						check(table, table.data('basictable'));
+					}
 				}
-				// Start responsive mode.
-				else if (options === 'start') {
-					start(table, table.data('basictable'));
-				}
-				else if (options === 'stop') {
-					end(table, table.data('basictable'));
-				}
-				else {
-					check(table, table.data('basictable'));
-				}
+				return false;
 			}
-			return false;
-		}
 
-		// Extend Settings.
-		var settings = $.extend({}, $.fn.basictable.defaults, options);
+			// Extend Settings.
+			var settings = $.extend({}, $.fn.basictable.defaults, options);
 
-		var vars = {
-			breakpoint: settings.breakpoint,
-			contentWrap: settings.contentWrap,
-			forceResponsive: settings.forceResponsive,
-			noResize: settings.noResize,
-			tableWrapper: settings.tableWrapper
-		};
+			var vars = {
+				breakpoint: settings.breakpoint,
+				contentWrap: settings.contentWrap,
+				forceResponsive: settings.forceResponsive,
+				noResize: settings.noResize,
+				tableWrap: settings.tableWrap,
+				showEmptyCells: settings.showEmptyCells,
+				baseClass: settings.baseClass
+			};
 
-		// Initiate
-		table.data('basictable', vars);
+			// Initiate
+			table.data('basictable', vars);
 
-		setup(table, table.data('basictable'));
+			setup(table, table.data('basictable'));
 
-		if (!vars.noResize) {
-			check(table, table.data('basictable'));
+			if (!vars.noResize) {
+				check(table, table.data('basictable'));
 
-			$(window).bind('resize.basictable', function() {
-				resize(table);
-			});
-		}
+				$(window).bind('resize.basictable', function() {
+					resize(table);
+				});
+			}
+		});
 	};
 
 	$.fn.basictable.defaults = {
@@ -4331,6 +4345,7 @@ This test will also return `true` for Firefox 4 Multitouch support.
 		forceResponsive: true,
 		noResize: false,
 		tableWrap: false,
+		showEmptyCells: false,
 		baseClass: 'bt'
 	};
 })(jQuery);
