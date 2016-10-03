3c631ee9205e1aa46307781db667585fcd54c03b
Velichko Konstantin <zoxon.box@gmail.com>
2016-10-03 09:25:04 +0600

    Add toggleable module

---
+++ source/modules/toggleable/toggleable.js
@@ -0,0 +1,58 @@
+// toggleable
+;(function ( $, window, document, undefined ) {
+	'use strict';
+
+	var $togglers = $('[data-toggleable-target]');
+
+	var toggleable = {
+		getState: function ($elem) {
+			var toggleState = $elem.data('toggleable-state');
+
+			if (typeof toggleState === 'undefined' || (toggleState != 'on' && toggleState != 'off')) {
+				toggleState = 'off';
+			}
+
+			return toggleState;
+		},
+
+		invertState: function (state) {
+			if (state == 'off') {
+				state = 'on';
+			}
+			else {
+				state = 'off'
+			}
+
+			return state;
+		},
+
+		applyState: function($target, state) {
+			$target.data('toggleable-state', state).trigger('update.toggleable');
+		},
+
+		setState: function ($button, $target, state) {
+			toggleable.applyState($button, state);
+			toggleable.applyState($target, state);
+		}
+	};
+
+	$togglers.each(function(index, el) {
+		var $thisButton = $(this);
+		var toggleState = toggleable.getState($thisButton);
+
+		var targetId = $thisButton.data('toggleable-target');
+		var $target = $('[data-toggleable-id="' + targetId + '"]');
+
+		// Set current state to target
+		toggleable.setState($thisButton, $target, toggleState);
+
+		$thisButton.on('click', function(event) {
+			var currentState = toggleable.getState($thisButton);
+			var state = toggleable.invertState(currentState);
+
+			toggleable.setState($thisButton, $target, state);
+
+		});
+	});
+
+})( jQuery, window, document );
