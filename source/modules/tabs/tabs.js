ed035f055b8d4a2f993e3f2302fe35fac25f483a
Velichko Konstantin <zoxon.box@gmail.com>
2016-12-14 09:24:37 +0600

    Update tabs module

--- source/modules/tabs/tabs.js
+++ source/modules/tabs/tabs.js
@@ -1,34 +1,63 @@
 // Tabs
 (function() {
 
-	var $tabsContainer = $('.tabs');
+	var $tabsWidgets = $('.tabs');
 
-	if ($tabsContainer.isset()) {
-		$tabsContainer.each(function() {
-			var $container = $(this);
-			var $tabs = $container.find('[data-tabs-target]');
-			var $panels = $container.find('[data-tabs-id]');
-			var $preloader = $container.find('.tabs__preloader');
+
+	if ($tabsWidgets.isset()) {
+		$tabsWidgets.each(function() {
+			var $tabWidget = $(this);
+			var $tabs = $tabWidget.find('[data-tabs-target]');
+			var tabsName = $tabWidget.data('tabs-name');
+			var $preloader = $tabWidget.find('.tabs__preloader');
+			var firstTabId = $tabs.eq(0).data('tabs-target');
 
 			if ($preloader) {
 				$preloader.hide();
 			}
 
-			// Show first tab
-			showTab($tabs.first());
+			showTab(tabsName, firstTabId);
+
+			$(window).on('hashchange onpopstate', function() {
+				var hash = window.location.hash.replace('#', '');
+
+				if (hash !== '') {
+					var hashData = parseHash(hash);
+
+					var $tabsContainer = $('[data-tabs-name="' + hashData.name + '"]');
+
+					if ($tabsContainer.isset()) {
+						showTab(hashData.name, hashData.id);
+					}
+				}
+			}).trigger('hashchange');
 
 			$tabs.on('click', function(event) {
 				event.preventDefault();
-				showTab($(this));
+				var id = $(this).data('tabs-target');
+
+				showTab(tabsName, id);
+
+				window.location.hash = tabsName + '__' + id;
 			});
 
-			function showTab($tab) {
-				var targetId = $tab.data('tabs-target');
-				var $relatedTabs = $container.find('[data-tabs-target="' + targetId + '"]');
-				var $targetPanel = $container.find('[data-tabs-id="' + targetId + '"]');
+			function parseHash(hash) {
+				var data = hash.split('__');
+				return {
+					'name': data[ 0 ],
+					'id': data[ 1 ]
+				};
+			}
+
+			function showTab(tabsName, id) {
+				var $tabsContainer = $('[data-tabs-name="' + tabsName + '"]');
+				var $tabs = $tabsContainer.find('[data-tabs-target]');
+				var $panels = $tabsContainer.find('[data-tabs-id]');
+				var $targetTab = $tabsContainer.find('[data-tabs-target="' + id + '"]');
+				var $targetPanel = $tabsContainer.find('[data-tabs-id="' + id + '"]');
 
 				$tabs.removeClass('tabs__tab_active');
-				$relatedTabs.addClass('tabs__tab_active');
+				$targetTab.addClass('tabs__tab_active');
 
 				$panels.removeClass('tabs__panel_active');
 				$targetPanel.addClass('tabs__panel_active');
