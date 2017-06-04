15a2e06974b3fa45b7776ee91f308729ad31af91
Velichko Konstantin <zoxon.box@gmail.com>
2017-06-04 22:35:50 +0600

    Add deploy task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,11 +2,13 @@ import { task, series } from 'gulp';
 import build from './gulp/tasks/build';
 import { zip } from './gulp/tasks/zip';
 import dev from './gulp/tasks/dev';
+import { deploy } from './gulp/tasks/deploy';
 
 // Main tasks
 task('build', build);
 task('zip', zip);
 task('dev', dev);
+task('deploy', deploy);
 
 // Semver
 import { major, minor, patch, reset } from './gulp/tasks/semver';
