833bc77312ffc218fe6c3a24f95b55ac3a1f5896
Velichko Konstantin <zoxon.box@gmail.com>
2017-08-04 22:47:45 +0600

    Update accordion plugin

--- source/static/scripts/main.js
+++ source/static/scripts/main.js
@@ -1,5 +1,7 @@
 import init from '../../modules/_utils/plugin-init';
 import Alert from '../../modules/alert/alert';
+import Accordion from '../../modules/accordion/accordion';
 
 init('.alert', Alert);
+init('.accordion', Accordion);
 
