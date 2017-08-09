a979fa1846674b90a6a1494ccb35acc23502f303
Velichko Konstantin <zoxon.box@gmail.com>
2017-08-09 14:55:14 +0600

    Fix errorHandler

--- gulp/util/errorHandler.js
+++ gulp/util/errorHandler.js
@@ -3,30 +3,29 @@ import notifier from 'node-notifier';
 import path from 'path';
 
 
-export default function errorHandler () {
-	return function (error) {
-		const date = new Date();
-		const cwd = process.cwd();
+export default function errorHandler (error) {
+	const date = new Date();
+	const cwd = process.cwd();
 
-		const now = date.toTimeString().split(' ')[0];
+	const now = date.toTimeString().split(' ')[0];
 
-		const title = error.name + ' in ' + error.plugin;
+	const title = error.name + ' in ' + error.plugin;
 
-		const shortMessage = error.message.split('\n')[ 0 ];
+	const shortMessage = error.message.split('\n')[ 0 ];
 
-		const message = '[' + colors.grey(now) + '] ' +
-			[ title.bold.red, '', error.message, '' ].join('\n');
+	const message = '[' + colors.grey(now) + '] ' +
+		[ title.bold.red, '', error.message, '' ].join('\n');
 
-		// Print message to console
-		// eslint-disable-next-line
-		console.log(message);
+	// Print message to console
+	// eslint-disable-next-line
+	console.log(message);
 
-		notifier.notify({
-			title: title,
-			message: shortMessage,
-			icon: path.join(cwd, 'tools/icons/error.svg')
-		});
+	notifier.notify({
+		title: title,
+		message: shortMessage,
+		icon: path.join(cwd, 'tools/icons/error.svg')
+	});
 
-		this.emit('end');
-	};
+
+	this.emit('end');
 }
