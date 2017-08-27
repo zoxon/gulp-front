ad4ba104162102ac8dc4dc25d6bec0dfca88b016
Velichko Konstantin <zoxon.box@gmail.com>
2017-08-27 12:59:37 +0600

    Update service worker task

--- gulp/tasks/serviceWorker.js
+++ gulp/tasks/serviceWorker.js
@@ -1,8 +1,11 @@
 import swPrecache from 'sw-precache';
+import { isDevelopment } from '../util/env';
 
 const serviceWorker = (callback) => {
   const rootDir = 'dest/';
   const config = {
+    // Determines whether the `fetch` event handler is included in the generated service worker code. It is useful to set this to `false` in development builds, to ensure that features like live reload still work. Otherwise, the content would always be served from the service worker cache.
+    handleFetch: !isDevelopment,
     staticFileGlobs: [
       rootDir + '**/*.{html,css,js}',
       rootDir + '**/*.{eot,ttf,woff,woff2}',
