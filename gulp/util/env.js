f96534b4f50275a34877606fae4ab06277e1ad5c
Velichko Konstantin <zoxon.box@gmail.com>
2017-10-01 14:57:16 +0600

    Add cross-env and update script section

--- gulp/util/env.js
+++ gulp/util/env.js
@@ -1,4 +1,2 @@
-import argv from './argv';
-
-export const NODE_ENV = (argv.production || argv.prod) ? 'production' : 'development';
+export const NODE_ENV = process.env.NODE_ENV ? 'production' : 'development';
 export const isDevelopment = NODE_ENV === 'development';
