adb891b91a9f3ad7da1fa930261ee1dafe709d90
Velichko Konstantin <zoxon.box@gmail.com>
2017-10-22 21:02:38 +0600

    Update Browsehappy

--- source/modules/browsehappy/browsehappy.js
+++ source/modules/browsehappy/browsehappy.js
@@ -1,8 +1,48 @@
-// browsehappy
-(function() {
+import hide from "../_utils/animations/hide";
+import cookie from "js-cookie";
 
-	$('.browsehappy').click(function() {
-		$(this).slideUp();
-	});
+export default class Browsehappy {
+  constructor(element, options) {
+    this.element = element;
+    this.name = "browsehappy";
 
-})();
+    this._defaults = {
+      single: true,
+      cookeName: "browsehappy-showed",
+      cookeExpires: 7
+    };
+
+    this.options = {
+      ...options,
+      ...this._defaults
+    };
+
+    this.init();
+  }
+
+  init() {
+    this.bindEvents();
+
+    if (this.options.single && !this.isShowed) {
+      this.element.style.display = "block";
+    }
+  }
+
+  bindEvents() {
+    this.element.addEventListener("click", event =>
+      this.onClick.call(this, event)
+    );
+    this.isShowed = cookie.get(this.options.cookeName) || false;
+  }
+
+  onClick(event) {
+    if (event.target.tagName.toUpperCase() === "A") {
+      return false;
+    }
+
+    hide(this.element);
+    cookie.set(this.options.cookeName, "true", {
+      expires: this.options.cookeExpires
+    });
+  }
+}
