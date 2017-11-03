26cc4178134f158a5db398e5f0474b67a900b430
Velichko Konstantin <zoxon.box@gmail.com>
2017-11-03 20:53:56 +0600

    Add emitty

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,80 +1,56 @@
-import gulp from 'gulp';
-import { reload } from './server';
-import { pages, html } from './html';
-import css from './css';
-import scripts from './scripts';
-import icons from './icons';
-import sprite from './sprite';
-import moduleImages from './moduleImages';
-import { assets, staticFiles } from './assets';
+import gulp from "gulp";
+
+import { reload } from "./server";
+import { pages, html } from "./html";
+import css from "./css";
+import scripts from "./scripts";
+import icons from "./icons";
+import sprite from "./sprite";
+import moduleImages from "./moduleImages";
+import { assets, staticFiles } from "./assets";
 
 const watch = () => {
+  global.watch = true;
 
   // Modules, pages
-  gulp.watch(
-    'source/**/*.pug',
-    gulp.series(pages, reload)
-  );
+  gulp
+    .watch("source/**/*.pug", gulp.series(pages, reload))
+    .on("all", (event, filepath) => {
+      global.emittyChangedFile = filepath;
+    });
 
   // Modules data
-  gulp.watch(
-    'source/modules/*/data/*.yml',
-    gulp.series(html, reload)
-  );
+  gulp.watch("source/modules/*/data/*.yml", gulp.series(html, reload));
 
   // Static styles
-  gulp.watch(
-    'source/static/styles/**/*.styl',
-    gulp.series(css)
-  );
+  gulp.watch("source/static/styles/**/*.styl", gulp.series(css));
 
   // Modules styles
-  gulp.watch(
-    'source/modules/**/*.styl',
-    gulp.series(css)
-  );
+  gulp.watch("source/modules/**/*.styl", gulp.series(css));
 
   // Static scripts
-  gulp.watch(
-    'source/static/scripts/**/*.js',
-    gulp.series(scripts, reload)
-  );
+  gulp.watch("source/static/scripts/**/*.js", gulp.series(scripts, reload));
 
   // Modules scripts
-  gulp.watch(
-    'source/modules/*/*.js',
-    gulp.series(scripts, reload)
-  );
+  gulp.watch("source/modules/*/*.js", gulp.series(scripts, reload));
 
   // Modules images
   gulp.watch(
-    'source/modules/*/images/*.{jpg,gif,svg,png}',
+    "source/modules/*/images/*.{jpg,gif,svg,png}",
     gulp.series(moduleImages, reload)
   );
 
   // Assets
-  gulp.watch(
-    'source/static/assets/**/*',
-    gulp.series(assets, reload)
-  );
+  gulp.watch("source/static/assets/**/*", gulp.series(assets, reload));
 
   // Svg icons
-  gulp.watch(
-    'source/static/icons/**/*.svg',
-    gulp.series(icons, css, reload)
-  );
+  gulp.watch("source/static/icons/**/*.svg", gulp.series(icons, css, reload));
 
   // Png sprites
-  gulp.watch(
-    'source/static/sprite/**/*.png',
-    gulp.series(sprite, reload)
-  );
+  gulp.watch("source/static/sprite/**/*.png", gulp.series(sprite, reload));
 
   // Static files
-  gulp.watch(
-    'source/static/public/**/{*,.*}',
-    gulp.series(staticFiles)
-  );
+  gulp.watch("source/static/public/**/{*,.*}", gulp.series(staticFiles));
 };
 
 export default watch;
