613d00a37b636b91118f6c3d7e3cb2bf2068be6e
Velichko Konstantin <zoxon.box@gmail.com>
2017-11-17 10:09:40 +0600

    Update removeDemo script

--- tools/removeDemo.js
+++ tools/removeDemo.js
@@ -1,64 +1,57 @@
 // This script removes demo app files
-import rimraf from 'rimraf';
-import fs from 'fs';
-import colors from 'colors';
+import rimraf from "rimraf";
+import fs from "fs";
+import colors from "colors";
 
 /* eslint-disable no-console */
 
 const pathsToRemove = [
-  './source/modules/demo',
-  './source/modules/header-presentation',
-  './source/modules/github-button',
-  './source/modules/header/*',
-  './source/modules/logo/logo.js',
-  './source/pages/home.pug',
-  './source/static/assets/favicons/*',
-  './source/static/assets/fonts/*',
-  './source/static/assets/images/content/*',
-  './source/static/assets/icons/*',
-  './source/static/assets/sprite/*',
-  './tools/removeDemo.js' // this file
+  "./source/modules/demo",
+  "./source/modules/header-presentation",
+  "./source/modules/github-button",
+  "./source/modules/sidebar",
+  "./source/modules/header/*",
+  "./source/modules/logo/logo.js",
+  "./source/pages/home.pug",
+  "./source/static/assets/favicons/*",
+  "./source/static/assets/fonts/*",
+  "./source/static/assets/images/content/*",
+  "./source/static/assets/icons/*",
+  "./source/static/assets/sprite/*",
+  "./tools/removeDemo.js" // this file
 ];
 
 const filesToCreate = [
   {
-    path: './source/pages/home.pug',
-    content:
-`include ../modules/injected/injected
-include ../modules/page/page
-
-include ../modules/head/head
+    path: "./source/pages/home.pug",
+    content: `extends ../layouts/default
 include ../modules/footer/footer
 include ../modules/header/header
 
-doctype html
-html(class="no-js" lang=config.lang)
 
-	head
-		+head(head.home)
+block head
+	- title = "Home page"
 
-	+page()
-		+inject('header')
-			+header()
+block header
+	+header()
 
-		+inject('main')
-			h1 Home page
+block main
+	h1 Home page
 
-		+inject('footer')
-			+footer()
+block footer
+	+footer()
 `
   },
   {
-    path: './source/modules/header/header.pug',
-    content:
-`mixin header(data)
+    path: "./source/modules/header/header.pug",
+    content: `mixin header(data)
 	header.header&attributes(attributes)
 		//-
 `
   },
   {
-    path: './source/modules/header/header.styl',
-    content: '.header\n\t//\n'
+    path: "./source/modules/header/header.styl",
+    content: ".header\n\t//\n"
   }
 ];
 
@@ -76,25 +69,25 @@ function createFile(file) {
 }
 
 function removePackageJsonScriptEntry(scriptName) {
-  const packageJsonPath = './package.json';
+  const packageJsonPath = "./package.json";
   let fileData = fs.readFileSync(packageJsonPath);
   let content = JSON.parse(fileData);
   delete content.scripts[scriptName];
-  fs.writeFileSync(packageJsonPath,
-  JSON.stringify(content, null, 2) + '\n');
+  fs.writeFileSync(packageJsonPath, JSON.stringify(content, null, 2) + "\n");
 }
 
 let numPathsRemoved = 0;
 pathsToRemove.map(path => {
   removePath(path, () => {
     numPathsRemoved++;
-    if (numPathsRemoved === pathsToRemove.length) { // All paths have been processed
+    if (numPathsRemoved === pathsToRemove.length) {
+      // All paths have been processed
       // Now we can create files since we're done deleting.
       filesToCreate.map(file => createFile(file));
     }
   });
 });
 
-removePackageJsonScriptEntry('remove-demo');
+removePackageJsonScriptEntry("remove-demo");
 
-console.log(colors.green('Demo app removed.'));
+console.log(colors.green("Demo app removed."));
