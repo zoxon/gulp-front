b5cecce31327e6a068232f3c34326cd14a58d9ef
Velichko Konstantin <zoxon.box@gmail.com>
2017-11-29 09:30:02 +0600

    Remove emitty
    Not working again

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,39 +3,23 @@ import plumber from "gulp-plumber";
 import pug from "gulp-pug";
 import posthtml from "gulp-posthtml";
 import prettify from "gulp-prettify";
-import gulpIf from "gulp-if";
-import { setup as emittySetup } from "emitty";
 
 import getJsonData from "../util/getJsonData";
 import { plumberConfig, posthtmlConfig, htmlPrettifyConfig } from "../config";
 import data from "./data";
 
-export const pages = () => {
+export function pages() {
   const pugConfig = {
     locals: getJsonData("./tmp/data.json")
   };
 
-  const emittyPug = emittySetup("source/pages", "pug");
-
-  return new Promise((resolve, reject) => {
-    return emittyPug
-      .scan(global.emittyChangedFile)
-      .then(() => {
-        gulp
-          .src(["**/*.pug", "!**/_*.pug"], { cwd: "source/pages" })
-          .pipe(plumber(plumberConfig))
-          .pipe(
-            gulpIf(global.watch, emittyPug.filter(global.emittyChangedFile))
-          )
-          .pipe(pug(pugConfig))
-          .pipe(posthtml(posthtmlConfig.plugins, posthtmlConfig.options))
-          .pipe(prettify(htmlPrettifyConfig))
-          .pipe(gulp.dest("dest"))
-          .on("end", resolve)
-          .on("error", reject);
-      })
-      .catch(err => console.log(err));
-  });
-};
+  return gulp
+    .src(["**/*.pug", "!**/_*.pug"], { cwd: "source/pages" })
+    .pipe(plumber(plumberConfig))
+    .pipe(pug(pugConfig))
+    .pipe(posthtml(posthtmlConfig.plugins, posthtmlConfig.options))
+    .pipe(prettify(htmlPrettifyConfig))
+    .pipe(gulp.dest("dest"));
+}
 
 export const html = gulp.series(data, pages);
