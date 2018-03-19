541acac132cda90fb7f07ab9199bc00a523bb375
Velichko Konstantin <zoxon.box@gmail.com>
2018-03-19 13:58:26 +0600

    Cleanup unused jquery code

--- source/static/scripts/vendor.js
+++ source/static/scripts/vendor.js
@@ -1,24 +1,2 @@
 // Console polyfill
 import "./helpers/console.polyfill.js";
-
-// FastclickJS
-// Remove delay between a physical tap and the firing of a click event on mobile browsers.
-// import FastClick from 'fastclick';
-// $(() => {
-// 	FastClick.attach(document.body);
-// });
-
-// SVG4Everybody
-// import svg4everybody from 'svg4everybody';
-// svg4everybody();
-
-// Helpers
-// import './plugins/helpers/jquery.isset.js';
-
-// Basic table
-// Responsive tables
-// import './plugins/basictable/jquery.basictable.js';
-
-// $(() => {
-// 	$('.table').basictable({ baseClass: 'table' });
-// });
