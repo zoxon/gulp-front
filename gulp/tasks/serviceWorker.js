ece1025ebb53908ba32a7e2a0c87251687e7e3e3
Velichko Konstantin <zoxon.box@gmail.com>
2018-04-25 07:50:06 +0600

    Add to gulp service-worker task logger

--- gulp/tasks/serviceWorker.js
+++ gulp/tasks/serviceWorker.js
@@ -1,10 +1,13 @@
+import path from "path";
 import swPrecache from "sw-precache";
+import logger from "gulplog";
 
 import { isDevelopment } from "../util/env";
 
 const serviceWorker = callback => {
   const rootDir = "dest/";
   const config = {
+    logger: logger.info,
     // Determines whether the `fetch` event handler is included in the generated service worker code. It is useful to set this to `false` in development builds, to ensure that features like live reload still work. Otherwise, the content would always be served from the service worker cache.
     handleFetch: !isDevelopment,
     staticFileGlobs: [
@@ -14,7 +17,7 @@ const serviceWorker = callback => {
     replacePrefix: "/"
   };
 
-  swPrecache.write("dest/precache.js", config, callback);
+  swPrecache.write(path.join(rootDir, "service-worker.js"), config, callback);
 };
 
 export default serviceWorker;
