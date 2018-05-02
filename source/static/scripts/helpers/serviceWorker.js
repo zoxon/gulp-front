2f2587d6cf6caf67669b56482ae2875d684062c7
Velichko Konstantin <zoxon.box@gmail.com>
2018-05-02 08:43:26 +0600

    Enable isLocalhost test in serviceWorker.js

--- source/static/scripts/helpers/serviceWorker.js
+++ source/static/scripts/helpers/serviceWorker.js
@@ -10,17 +10,15 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
-// const isLocalhost = Boolean(
-//   window.location.hostname === "localhost" ||
-//     // [::1] is the IPv6 localhost address.
-//     window.location.hostname === "[::1]" ||
-//     // 127.0.0.1/8 is considered localhost for IPv4.
-//     window.location.hostname.match(
-//       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-//     )
-// );
-
-const isLocalhost = false;
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
 
 export function register(config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
