849cb626f54864feacccf9111335cafe67fda54d
Konstantin Velichko <zoxon@Zoxon.local>
2018-07-02 10:24:13 +0600

    Update modal and dialog modules

--- source/modules/modal/modal.js
+++ source/modules/modal/modal.js
@@ -1,7 +1,34 @@
 import MicroModal from "micromodal";
 
+const execYoutubeCommand = (frame, command) => {
+  if (null === frame.contentWindow) {
+    return;
+  }
+
+  frame.contentWindow.postMessage(
+    window.JSON.stringify({ event: "command", func: command }),
+    "https://www.youtube.com"
+  );
+};
+
+const modalHandler = (modal, command) => {
+  const iframes = modal.querySelectorAll("iframe");
+
+  [].forEach.call(iframes, frame => {
+    execYoutubeCommand(frame, command);
+  });
+};
+
 export default () => {
   MicroModal.init({
-    awaitCloseAnimation: true
+    awaitCloseAnimation: true,
+    onClose: modal => {
+      modalHandler(modal, "pauseVideo");
+      document.documentElement.classList.remove("scroll-locked");
+    },
+    onShow: modal => {
+      modalHandler(modal, "playVideo");
+      document.documentElement.classList.add("scroll-locked");
+    }
   });
 };
