3b4126913fddd69b53141a410514ba55da00bb43
Konstantin Velichko <zoxon@Zoxon.local>
2018-07-06 16:07:54 +0600

    Update accordion module
    New pug mixin
    Update styles

--- source/modules/accordion/accordion.js
+++ source/modules/accordion/accordion.js
@@ -6,7 +6,8 @@ class Accordion extends Plugin {
     return {
       itemSelector: `[data-accordion-item]`,
       triggerSelector: `[data-accordion-trigger]`,
-      itemActiveAttr: `data-accordion-item`,
+      panelSelector: `[data-accordion-panel]`,
+      itemActiveAttr: `data-accordion-item-open`,
       singleOpen: true
     };
   }
@@ -14,9 +15,38 @@ class Accordion extends Plugin {
   buildCache() {
     this.items = toArray(
       this.element.querySelectorAll(this.options.itemSelector)
-    ).filter(
-      item => item.getAttribute(this.options.itemActiveAttr) !== "disabled"
+    ).filter(item => {
+      const isDisabled = this.isItemDisabled(item);
+
+      if (isDisabled) {
+        this.setItemDisabled(item);
+      }
+
+      return !isDisabled;
+    });
+  }
+
+  isItemDisabled(item) {
+    return (
+      item.getAttribute("aria-disabled") === "true" ||
+      item.hasAttribute("disabled") ||
+      false
     );
+  }
+
+  getTrigger(item) {
+    return item.querySelector(this.options.triggerSelector);
+  }
+
+  getPanel(item) {
+    return item.querySelector(this.options.panelSelector);
+  }
+
+  setItemDisabled(item) {
+    const trigger = this.getTrigger(item);
+
+    trigger.setAttribute("disabled", true);
+    this.element.setAttribute("aria-disabled", true);
   }
 
   bindEvents() {
@@ -30,11 +60,20 @@ class Accordion extends Plugin {
   }
 
   open(item) {
-    item.setAttribute(this.options.itemActiveAttr, true);
+    this.toggleVisibility(item, true);
   }
 
   close(item) {
-    item.setAttribute(this.options.itemActiveAttr, false);
+    this.toggleVisibility(item, false);
+  }
+
+  toggleVisibility(item, visibility) {
+    const trigger = this.getTrigger(item);
+    const panel = this.getPanel(item);
+
+    item.setAttribute(this.options.itemActiveAttr, visibility);
+    trigger.setAttribute("aria-expanded", visibility);
+    panel.setAttribute("aria-hidden", !visibility);
   }
 
   toggle(item) {
