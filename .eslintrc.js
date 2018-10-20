33fdd29264633b40e199452899af578bd291e58f
Velichko Konstantin <zoxon.box@gmail.com>
2018-10-20 17:10:03 +0600

    Update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,27 +6,29 @@ module.exports = {
     "standard",
     "plugin:import/errors",
     "plugin:import/warnings",
+    "plugin:jsx-a11y/recommended",
+    "plugin:jest/recommended",
     "prettier",
     "prettier/standard"
   ],
-  plugins: ["import", "prettier", "standard"],
+  plugins: ["import", "jsx-a11y", "jest", "prettier", "standard"],
   globals: {
     mockApi: true,
     nock: true
   },
   env: {
+    "jest/globals": true,
     browser: true,
     commonjs: true,
     es6: true,
-    node: true,
-    jquery: true,
-    jest: true
+    jest: true,
+    node: true
   },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
-      experimentalObjectRestSpread: true,
+      jsx: true,
       generators: true
     }
   },
@@ -228,7 +230,32 @@ module.exports = {
     // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
     "import/first": "error",
     "import/no-amd": "error",
-    "import/no-webpack-loader-syntax": "error"
+    "import/no-webpack-loader-syntax": "error",
+
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
+    "jsx-a11y/accessible-emoji": "warn",
+    "jsx-a11y/alt-text": "warn",
+    "jsx-a11y/anchor-has-content": "warn",
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        aspects: ["noHref", "invalidHref"]
+      }
+    ],
+    "jsx-a11y/aria-activedescendant-has-tabindex": "warn",
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-role": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/heading-has-content": "warn",
+    "jsx-a11y/iframe-has-title": "warn",
+    "jsx-a11y/img-redundant-alt": "warn",
+    "jsx-a11y/no-access-key": "warn",
+    "jsx-a11y/no-distracting-elements": "warn",
+    "jsx-a11y/no-redundant-roles": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+    "jsx-a11y/scope": "warn"
   },
   settings: {
     "import/resolver": {
